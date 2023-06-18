4977078b91069acbce59ccc7e1db981faf5afd66
frazer <sdfrazer@gmail.com>
2023-06-18 19:27:00 -0400

    fix the deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} / travis@137.184.160.164:/home/travis/demo/client
-  rsync -a server/ travis@137.184.160.164:/home/travis/demo/server
+  rsync -a --exclude={'/node_modules','/src','/public'} / root@137.184.160.164:/home/travis/demo/client
+ 
 else
   echo "Not deploying, since the branch isn't master."
 fi
