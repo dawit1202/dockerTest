82f2d1805c232c6168d7a7255a69e10200d0b4df
frazer <sdfrazer@gmail.com>
2023-06-18 19:22:29 -0400

    remove the client folder refrence from deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@137.184.160.164:/home/travis/demo/client
+  rsync -a --exclude={'/node_modules','/src','/public'} / travis@137.184.160.164:/home/travis/demo/client
   rsync -a server/ travis@137.184.160.164:/home/travis/demo/server
 else
   echo "Not deploying, since the branch isn't master."
