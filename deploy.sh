89f2c87e41de16c5734b7a03707114050bcd38f3
frazer <sdfrazer@gmail.com>
2023-06-18 19:18:16 -0400

    configure travis plus digital ocean

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@<droplet ipaddress>:/home/<sudo user>/demo/client
-  rsync -a server/ travis@<droplet ipaddress>:/home/<sudo user>/demo/server
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@137.184.160.164:/home/travis/demo/client
+  rsync -a server/ travis@137.184.160.164:/home/travis/demo/server
 else
   echo "Not deploying, since the branch isn't master."
 fi
