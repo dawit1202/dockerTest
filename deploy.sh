93da141d9c568555490b8924175fda9dbcac0bb8
frazer <sdfrazer@gmail.com>
2023-06-18 22:29:14 -0400

    create a travis user

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -av --exclude={'/node_modules','/src','/public'} ./ root@137.184.160.164:~/xyz/demo
+  rsync -av --exclude={'/node_modules','/src','/public'} ./ travis@137.184.160.164:~/test/demo
  
 else
   echo "Not deploying, since the branch isn't master."
