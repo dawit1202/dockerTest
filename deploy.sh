06788328b2c9bb5dc0d13f70428e7d4f9899f0fd
frazer <sdfrazer@gmail.com>
2023-06-18 21:26:47 -0400

    add the ip address of droplet to travis

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} ./ newtravis@137.184.160.164:/home/newtravis/demo
+  rsync -a --exclude={'/node_modules','/src','/public'} ./ newtravis@137.184.160.164:/home/xyz/demo
  
 else
   echo "Not deploying, since the branch isn't master."
