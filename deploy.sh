6b83560a3128bc62688211dddb88f1ba894cd380
frazer <sdfrazer@gmail.com>
2023-06-19 23:12:47 -0400

    include src folder

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -av --exclude={'/node_modules','/src'} ./ travis@137.184.160.164:~/demo
+  rsync -av --exclude={'/node_modules'} ./ travis@137.184.160.164:~/demo
  
 else
   echo "Not deploying, since the branch isn't master."
