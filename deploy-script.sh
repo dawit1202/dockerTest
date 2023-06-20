e1759fc160b9a6b4401381cbe305fe07cf5f3b03
frazer <sdfrazer@gmail.com>
2023-06-19 23:07:52 -0400

    include public folder

--- deploy-script.sh
+++
@@ -1,7 +0,0 @@
-
-# Pull the latest Docker image and restart the container
-docker pull  ldawit/docker-react
-docker ps --format "{{.Names}}" as cname 
-docker stop cname
-docker rm cname
-docker run -d -p 80:80 --name cname  ldawit/docker-react
